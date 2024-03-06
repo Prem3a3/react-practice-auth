cf84eab54a900d683eaf6cb0a3eb3c135e8b5f54
prem chandu <prem@gmail.com>
2024-03-06 20:58:03 +0530

    done two way data-binding

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { Header } from './header';
-import { User } from './User';
+import { useState } from "react";
 
-let s1 = {
-  background: "white",
-  color: "black",
-  marginTop: "200px",
-  border: "100px"
-}
+export function App(){
 
-export function displayMessage(number){
-  alert( `{userDetails.filter((s) => number ===s.id)}`)
-}
+  let num = 500;
+  let [nums, updateNum] = useState(100)
 
-const userDetails = [
-  {
-    id: 20,
-    name: 'Abhraham',
-    place: 'Ohio'
-  },
-  {
-    id: 21,
-    name: 'Revanth',
-    place: 'miami'
-  },
-  {
-    id: 22,
-    name: 'Kranthi',
-    place: 'miami'
-  }
-]
-
-export function App() {
-  
-  return (
-    <button onClick={displayMessage}>display</button>
-  )
-}
-
-function Fragments() {
   return (
     <>
-      <div>This is div1</div>
-      <div>This is div2</div>
-      <div>This is div2</div>
-
-    </>
+    <button onClick={() => {
+      num = 200;
+      alert(num)
+    }}> click</button>
+    <br></br>
+    <input type='text' onChange={(e) => {
+      let newNum = e.target.value;
+      updateNum(newNum);
+    }} value={num}/>
+     <p>{nums}</p></>
+   
   )
 }
